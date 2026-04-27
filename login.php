2905c04a9f5473494e641f51f3a3a90f881ecf9f
GHZIRI <salmantanger1234@gmail.com>
2026-04-27 23:20:36 +0200

    add page login html

---
+++ login.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Login</h1>
+     <form action="">
+        <label for="">Name</label>
+        <input type="text" name="name" id=""><br>
+        <label for="">Email</label>
+        <input type="email" name="email" id=""><br>
+        <label for="">Password</label>
+        <input type="password" name="poassword" id=""><br>
+        <button type="submit" name="clicke">login</button>
+     </form>
+</body>
+</html>
